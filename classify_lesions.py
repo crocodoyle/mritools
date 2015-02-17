e383ad1bfa234ce253fb85461b9091a21ec41861
Andrew Doyle <adoyle@cim.mcgill.ca>
2015-02-17 14:37:34 -0500

    less scales

--- classify_lesions.py
+++ classify_lesions.py
@@ -26,49 +26,61 @@ for root, dirs, filenames in os.walk(data_dir):
         if f.endswith('_m0_t1p.mnc.gz'):
             mri_list.append(mri(f))
 
-f = h5py.File(data_dir + 'features.hdf5', 'r')
+f = h5py.File(data_dir + 'features2.hdf5', 'r')
 
 #features types: malf-context*scales + image-intensities
 #features types: 10*5 + 4
-training_vector = np.zeros(shape=(15, 60*256*256*54))
+
+image_pixels = 60*256*256
+
+priors = 10
+feature_scales = 2
+num_mods = 4
+
+num_train = 15
+num_test = 5
+
+
+training_vector = np.zeros(shape=(num_train, image_pixels*(priors*feature_scales+num_mods)))
 print 'train_vector', np.shape(training_vector)
 
-for i, img in enumerate(mri_list[0:15]):
+for i, img in enumerate(mri_list[0:num_train]):
+    print i
     features = f[img.uid]
-    flat_feature = np.reshape(features, 60*256*256*50)
+    flat_feature = np.reshape(features, image_pixels*feature_scales*priors)
     #print 'flat_features', np.shape(flat_feature)
-    training_vector[i, 0:60*256*256*50] = flat_feature
+    training_vector[i, 0:image_pixels*(feature_scales*priors)] = flat_feature
     #print 'begin ', 0:60*256*256*50
     
     for j, m in enumerate(modalities):
         malf_data = nib.load(img.images[m]).get_data()
-        flat_malf_data = np.reshape(malf_data, (60*256*256))
+        flat_malf_data = np.reshape(malf_data, (image_pixels))
         #print 'flat_malf', np.shape(flat_malf_data)
         #print 'train_vector_slice', np.shape(training_vector[i, 60*256*256*(50+j):60*256*256*(50+j+1)])
         
         #print 'be gin: ', 60*256*256*(50+j+1)
         #print 'end: ', 60*256*256*(50+j+2)
         
-        training_vector[i, 60*256*256*(50+j):60*256*256*(50+j+1)] = flat_malf_data
+        training_vector[i, image_pixels*(feature_scales*priors+j):image_pixels*(feature_scales*priors+j+1)] = flat_malf_data
     
-test_vector = np.zeros(shape=(5, 60*256*256*54))
-for i, img in enumerate(mri_list[15:20]):
+test_vector = np.zeros(shape=(5, image_pixels*(feature_scales*priors + num_mods)))
+for i, img in enumerate(mri_list[num_train:num_train+num_test]):
     features = f[img.uid]
-    test_vector[i, 0:60*256*256*50] = np.reshape(features, 60*256*256*50)
+    test_vector[i, 0:image_pixels*(feature_scales*priors)] = np.reshape(features, image_pixels*feature_scales*priors)
     
     for j, m in enumerate(modalities):
         malf_data = nib.load(img.images[m]).get_data()
-        flat_malf_data = np.reshape(malf_data, (60*256*256))
-        test_vector[i, 60*256*256*(50+j):60*256*256*(50+j+1)] = flat_malf_data
+        flat_malf_data = np.reshape(malf_data, (image_pixels))
+        test_vector[i, image_pixels*(feature_scales*priors+j):image_pixels*(feature_scales*priors+j+1)] = flat_malf_data
 
 
-train_labels = np.zeros(shape=(15, 60*256*256))
-for i, img in enumerate(mri_list[0:15]):
-    train_labels[i, :] = nib.load(img.lesions).get_data()
+train_labels = np.zeros(shape=(num_train, image_pixels))
+for i, img in enumerate(mri_list[0:num_train]):
+    train_labels[i, :] = np.reshape(nib.load(img.lesions).get_data(), image_pixels)
 
-test_labels = np.zeros(shape=(5, 60*256*256))
-for i, img in enumerate(mri_list[15:20]):
-    test_labels[i, :] = nib.load(img.lesions).get_data()
+test_labels = np.zeros(shape=(num_test, image_pixels))
+for i, img in enumerate(mri_list[num_train:num_train+num_test]):
+    test_labels[i, :] = np.reshape(nib.load(img.lesions).get_data(), image_pixels)
 
 
 
--- context_extraction.py
+++ context_extraction.py
@@ -15,7 +15,6 @@ data_dir = 'C:/MRI/MS-LAQ/'
 
 malf_classes = ['bg', 'bs', 'cgm', 'crblr_gm', 'crblr_wm', 'csf', 'dgm', 'lv', 'ov', 'wm']
 modalities = ['t1p', 't2w', 'pdw', 'flr']
-good_malf_classes = ['cgm', 'dgm', 'wm']
 
 
 mri_list = []
@@ -29,14 +28,15 @@ for root, dirs, filenames in os.walk(data_dir):
 #malf_thresh = {}
 malf_tissues = {}
 
-scales = [1,2,3,4,5]
+scales = [1,5]
 
 
-size = nib.load(mri_list[0].priors['t1p'])
+size = nib.load(mri_list[0].priors['cgm'])
 
 
 feature_map = np.zeros(shape=(np.shape(size)[0], np.shape(size)[1], np.shape(size)[2], len(malf_classes), len(scales)))
 
+print len(malf_classes)
 
 f = h5py.File(data_dir + "features2.hdf5", "w")
 for i, img in enumerate(mri_list):
@@ -44,13 +44,12 @@ for i, img in enumerate(mri_list):
 
     tissues = {}
     for mod in modalities:
-        tissues[mod] = nib.load(img.priors[mod]).get_data()
+        tissues[mod] = nib.load(img.images[mod]).get_data()
 
     for j, m in enumerate(malf_classes):
-        malf_tissues[m] = nib.load(img.malf[m]).get_data()
-#        malf_thresh[m] = np.greater_equal(malf_tissues[m], 0.7)
-        for s in scales:
-            feature_map[:,:,:,j,s-1] = convolve(malf_tissues[m],np.ones((s,s,s)))
+        malf_tissues[m] = nib.load(img.priors[m]).get_data()
+        for k, s in enumerate(scales):
+            feature_map[:,:,:,j,k] = convolve(malf_tissues[m],np.ones((s,s,s)))
     
     dset = f.create_dataset(img.uid, np.shape(feature_map), dtype='f', compression="gzip")
     dset[...] = feature_map
