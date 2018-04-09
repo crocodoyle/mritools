c6fb03ff84d36eb9dfd3c562953bcfedb23827fb
Andrew Doyle <andrew.doyle@mcgill.ca>
2018-04-09 16:49:58 -0400

    bug

--- feature_extraction.py
+++ feature_extraction.py
@@ -225,15 +225,15 @@ def get_rift(scan, img):
                     o = np.sin(arrow_angle)*(max_grad_val / 100)*5
                     a = np.cos(arrow_angle)*(max_grad_val / 100)*5
 
-                    arrow_begin = (max_grad_pos[0], max_grad_pos[1])
-                    arrow_end = (a, o)
+                    arrow_begin = (40-max_grad_pos[0], 40-max_grad_pos[1])
+                    arrow_end = (-a, -o)
 
                     print('arrow begin:', arrow_begin, 'arrow end:', arrow_end)
 
                     angle_img = ax4.imshow(angle[int(xc), int(yc) - 20: int(yc) + 20, int(zc) - 20: int(zc) + 20], cmap=plt.cm.gray, interpolation='nearest', origin='lower')
                     fig.colorbar(angle_img, ax=ax4)
 
-                    ax4.arrow(arrow_begin[0], arrow_begin[1], arrow_end[0], arrow_end[1], head_width=0.05, head_length=0.1, color='b')
+                    ax4.arrow(arrow_begin[0], arrow_begin[1], arrow_end[0], arrow_end[1], head_width=0.25, head_length=0.5, color='b')
 
                     ax4.plot(centre_point[0], centre_point[1], 'ro', markersize=2)
                     ax4.plot(arrow_begin[0], arrow_begin[1], 'bo', markersize=2)
