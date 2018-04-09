060864def14bad6d6e2e8e7298fb78689dc6b2e7
Andrew Doyle <andrew.doyle@mcgill.ca>
2018-04-09 15:22:48 -0400

    bug

--- feature_extraction.py
+++ feature_extraction.py
@@ -221,16 +221,19 @@ def get_rift(scan, img):
 
                     arrow_angle = max_grad_angle + np.arctan2((max_grad_pos[0] - yc), (max_grad_pos[1] - zc))
 
-                    o = np.sin(arrow_angle)*(max_grad_val / 10000)
-                    a = np.cos(arrow_angle)*(max_grad_val / 10000)
+                    o = np.sin(arrow_angle)*(max_grad_val / 100000)
+                    a = np.cos(arrow_angle)*(max_grad_val / 100000)
+
+                    print('o', o)
+                    print('a', a)
 
                     arrow_begin = (max_grad_pos[0], max_grad_pos[1])
                     arrow_end = arrow_begin + (a, o)
 
-                    ax4.arrow(arrow_begin[0], 40-arrow_begin[1], arrow_end[0], arrow_end[1], head_width=0.05, head_length=0.1, color='b')
+                    ax4.arrow(40-arrow_begin[0], 40-arrow_begin[1], arrow_end[0], arrow_end[1], head_width=0.05, head_length=0.1, color='b')
 
                     ax4.plot(centre_point[0], centre_point[1], 'ro', markersize=2)
-                    ax4.plot(arrow_begin[0], 40-arrow_begin[1], 'bo', markersize=2)
+                    ax4.plot(40-arrow_begin[0], 40-arrow_begin[1], 'bo', markersize=2)
 
                     ax4.set_xticks([])
                     ax4.set_yticks([])
